1c9b44caae2a8c51671c35fc5032dcc6c024664e
Bin <blponio@memphis.edu>
2024-03-15 15:24:28 -0500

    Completed all tests for Brie cases

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -67,6 +67,11 @@ class GildedRose
     @quality -= 1 if @days_remaining <= 0
   end
   def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0 && @quality < 50
   end
   end
 end
